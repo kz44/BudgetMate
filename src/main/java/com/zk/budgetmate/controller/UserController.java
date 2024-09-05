cb44ecd832ae5dbd851a31b672a48199ad58f9da
Kollonay Zoltán <zoltan@kollonay.hu>
2024-09-05 10:45:21 +0200

    feat: added Controller classes

---
+++ src/main/java/com/zk/budgetmate/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.zk.budgetmate.controller;
+
+import com.zk.budgetmate.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+  private final UserService userService;
+}
