7ee4b9cb0947a007e3739f23cb2dabbc49fb5b39
Kollonay Zoltán <zoltan@kollonay.hu>
2024-09-15 12:13:10 +0200

    feat: liquibase works properly

--- src/main/resources/db.changelog.versions/0.0.1/invoices/add_new_invoice.sql
+++ src/main/resources/db.changelog.versions/0.0.1/invoices/add_new_invoice.sql
@@ -2,22 +2,22 @@
 --changeset zkollonay:add_new_invoice splitStatements:true endDelimiter:;
 
 insert into invoices (name, balance, user_id) --IMI
-values ('Loans and Debts', '100', 1);
+values ('Loans and Debts', 100, 1);
 
 insert into invoices (name, balance, user_id) --IMI
-values ('Rent', '180000', 1);
+values ('Rent', 180000, 1);
 
 insert into invoices (name, balance, user_id) --ZÉ
-values ('Savings', '1000000', 2);
+values ('Savings', 1000000, 2);
 
 insert into invoices (name, balance, user_id) --ZÉ
-values ('Holiday', '500000', 2);
+values ('Holiday', 500000, 2);
 
 insert into invoices (name, balance, user_id) --Zalántai
-values ('Insurance', '440000', 3);
+values ('Insurance', 440000, 3);
 
 insert into invoices (name, balance, user_id) --Csecse
-values ('Wedding', '2875000', 4);
+values ('Wedding', 2875000, 4);
 
 insert into invoices (name, balance, user_id) --Csecse
-values ('Household Expenses', '325000', 4);
+values ('Household Expenses', 325000, 4);
