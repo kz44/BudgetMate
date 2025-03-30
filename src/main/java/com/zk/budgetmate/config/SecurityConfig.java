733725954daf96feb9808e535d518646c0896189
Kollonay Zoltán <zoltan@kollonay.hu>
2025-03-30 10:37:58 +0200

    feat: added jwt decoder

--- src/main/java/com/zk/budgetmate/config/SecurityConfig.java
+++ src/main/java/com/zk/budgetmate/config/SecurityConfig.java
@@ -4,12 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+  @Bean
+  public JwtDecoder jwtDecoder() {
+    return JwtDecoders.fromIssuerLocation("https://dev-bjg0ei8c7tjnsovx.us.auth0.com/");
+  }
+
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,7 +27,8 @@ public class SecurityConfig {
             .requestMatchers("/api/public/**").permitAll()
             .anyRequest().authenticated()
         )
-        .oauth2ResourceServer().jwt();
+        .oauth2ResourceServer(oauth2 -> oauth2
+            .jwt(jwt -> jwt.decoder(jwtDecoder())));
 
     return http.build();
   }
